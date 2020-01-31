5812eab26002cb783eae491925b8e34bf8b7ae48
candyapplecorn@gmail.com <candyapplecorn@gmail.com>
2020-01-30 21:36:56 -0800

    Version model now handles trailing zeros and sorts

--- app/models/version.js
+++ app/models/version.js
@@ -1,11 +1,17 @@
 class Version {
   constructor(versionString) {
     this.versionString = versionString;
-    this.version = this.parseVersion(versionString);
+    this.version = Version.parseVersion(
+      Version.removeTrailingZeroes(versionString)
+    );
   }
 
-  parseVersion(versionString) {
+  static parseVersion(versionString) {
     return versionString.split('.').map(Number);
+  }
+
+  static removeTrailingZeroes(versionString) {
+    return versionString.replace(/(\.0+)+$/, '');
   }
 
   static isValidVersion(versionString) {
@@ -14,10 +20,36 @@ class Version {
 
   static greatestVersion(versionList) {
     const versions = versionList.map(version => new Version(version));
+    const sorted = versions.sort(Version.versionComparator);
+    return sorted[0].versionString
   }
 
   static versionComparator(a, b) {
+    if (a.version.join('') === b.version.join('')) {
+      return 0;
+    }
 
+    const minLength = Math.min(a.version.length,b.version.length);
+
+    for (var i = 0; i < minLength; i++) {
+      const [digitA, digitB] = [a.version[i], b.version[i]];
+
+      if (digitA <= digitB) {
+        return 1;
+      } else if (digitA > digitB) {
+        return -1;
+      }
+
+      if (i === minLength - 1) {
+          const [nextA, nextB] = [a.version[i + 1], b.version[i + 1]];
+
+          if (nextA > 0) {
+            return -1
+          } else if (nextB > 0) {
+            return 1;
+          }
+      }
+    }
   }
 }
 
--- app/models/version.test.js
+++ app/models/version.test.js
@@ -1,6 +1,43 @@
 const Version = require('./version');
 
 describe('Version', () => {
+    it('should have a version', () => {
+        const version = new Version('2.1.3');
+        expect(version.version).toEqual([2, 1, 3]);
+    });
+
+    it('should have a version string', () => {
+        const versionString = '2.3.1.2.1.03.2.33333';
+        const version = new Version(versionString);
+        expect(version.versionString).toEqual(versionString);
+    });
+
+    describe('removeTrailingZeroes', () => {
+        it.each([
+          ['1.0.0.0', '1'],
+          ['1.2', '1.2'],
+          ['1.2.0.1.0', '1.2.0.1']
+        ])('should remove the trailing zeroes from %s to equal %s', (ver, expected) => {
+            expect(Version.removeTrailingZeroes(ver)).toEqual(expected)
+        });
+    });
+
+    describe('greatestVersion', () => {
+        it.each([
+            [['1', '2'], '2'],
+            [['1', '2', '1.9'], '2'],
+            [['1', '2', '1.9'], '2'],
+            [['0.0.1', '0.1'], '0.1'],
+            [['0.0.1', '0.0.1.09'], '0.0.1.09'],
+            [['10', '3', '0.0.1', '0.0.1.09'], '10'],
+        ])(
+          'it should determine the greatest version from %o is %s',
+          (list, greatest) => {
+            const shouldBeGreatest = Version.greatestVersion(list);
+            expect(shouldBeGreatest).toEqual(greatest);
+        });
+    });
+
     describe('validity', () => {
       it.each([
         ['2', 'VALID'],
@@ -10,6 +47,7 @@ describe('Version', () => {
         ['', 'INVALID'],
         ['.1.0', 'INVALID'],
         ['0.1.0', 'VALID'],
+        ['-1', 'INVALID'],
       ])('should be able to tell %s is %s', (ver, validity) => {
         validity = (validity === 'VALID');
         expect(Version.isValidVersion(ver)).toEqual(validity);
